9d65249c79bf952ff35fe5d16dca8f3c8b63b0c5
A1000 <105562940+AyoubSemsar@users.noreply.github.com>
2022-08-13 11:44:12 +0200

    Update api.py

--- api.py
+++ api.py
@@ -4,6 +4,10 @@ from typing import Union
 from pydantic import BaseModel
 from typing import Optional
 import couchdb
+import requests
+import json
+
+
 
 api = FastAPI() #on instancie 
 
@@ -13,20 +17,33 @@ class User_register(BaseModel):
     last_name: str
     gender: str
     age: str
-    address: str
+    country: str
     e_mail: str
+    password: str
 
 
 
-
-@api.post('/users') 
-async def sign_up_user(info_user: User_register):
-    couch_server = couchdb.Server("http://admin:kolea21342@localhost:5984/")
-    couch_db = couch_server.create("my_database_2")
-    couch_db.save(info_user.dict())
+@api.post('/test')
+async def sign_up(info_user: User_register):
+    db = couchdb.Database('http://admin:kolea21342@localhost:5984/my_database_2')
+    db.save(info_user.dict())
     return {"Status":"Done"}
 
 
+@api.post('/test_verification')
+async def test_verification(info_user: User_register):
+    db = couchdb.Database('http://admin:kolea21342@localhost:5984/my_database_2')
+    info_user = info_user.dict()
+    if len(info_user["password"]) < 8:
+        return {"bad":"password"}
+    else:
+        db.save(info_user)
+        return {"Status":"Done"}
+
+@api.get('/login')
+async def login_user():
+    return {"Hello":"World"}
+
 
 if __name__ == '__main__':
-    uvicorn.run(api, host= '127.0.0.1', port= 8000)
+    uvicorn.run(api, host= '127.0.0.1', port= 2222)
