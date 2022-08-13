c5c275a6ce67292052088b5c1a5909d121034dda
A1000 <105562940+AyoubSemsar@users.noreply.github.com>
2022-08-13 11:45:32 +0200

    Update ReviewinApp.py

--- ReviewinApp.py
+++ ReviewinApp.py
@@ -2,7 +2,6 @@ from cProfile import run
 from math import fabs
 from os import access
 from pickle import TRUE
-import re
 from turtle import onclick
 from unittest import removeResult
 from kivy.uix.screenmanager import ScreenManager, Screen 
@@ -32,17 +31,27 @@ from kivy.app import App
 from kivy.animation import Animation
 from kivy.uix.image import Image
 import requests
-from kivymd.app import App
+from kivy.properties import ObjectProperty 
+import re 
+import json
+import random2
+import random
 
+
+
+pattern = '^[a-z 0-9]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$'
+
+#fonts
 LabelBase.register(name="OpenSans",fn_regular="OpenSans-Bold.ttf")
 LabelBase.register(name="RSlab", fn_regular="RobotoSlab-VariableFont_wght.ttf")
 LabelBase.register(name= "Popp",fn_regular="FontsFree-Net-Poppins-Bold.ttf")
-LabelBase.register(name= "Inter", fn_regular= "FontsFree-Net-Inter-Regular.ttf")
+LabelBase.register(name= "Inter", fn_regular= "FontsFree-Net-Inter-Regular.ttf") 
 
-something = True 
+
 
 Window.size = (360,800)
  
+#principale classe de l'application
 class ReviewinApp(MDApp):
     def build(self):
         dialog = None
@@ -59,36 +68,11 @@ class ReviewinApp(MDApp):
         sm.add_widget(Builder.load_file("rules.kv"))
         sm.add_widget(Builder.load_file("commentinput.kv"))
         sm.add_widget(Builder.load_file("UserInfo.kv"))
+        sm.add_widget(Builder.load_file("recaptcha.kv"))
         return sm
 
-
-    def check_register(self, text):
-        if len(text) < 5:
-            toast("Please register correctly.")
-        else:
-            return text
-
-    def check_register_e_mail(self, text):
-        if not "@" and '.' in text and len(text) < 3:
-            toast("Please give a valid e_mail")
-        else:
-            return text
-
-
-    def check_register_address(self, text):
-        if len(text) < 10:
-            toast("Please enter a valid address")
-        else:
-            return text
-
-    def check_register_password(self, text):
-        if len(text) < 8 and not "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" in text:
-            toast("Your password must contain at least 8 characters and at least one number.")
-        else:
-            return text
-
     def on_start(self):
-        Clock.schedule_once(self.accueil, 10)
+        Clock.schedule_once(self.accueil, 30)
         
     def accueil(*args):
         sm.current = "accueil"
@@ -133,44 +117,148 @@ class ReviewinApp(MDApp):
 
     def stay_app(self):
         self.dialog.dismiss()
- 
-    def check_text(self):
-        pattern = '^[a-z 0-9]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$'
-        e_mail = self.root.current_screen.ids.e_mail.text
-        age = int(self.root.current_screen.ids.age.text)
-        full_name = self.root.current_screen.ids.full_name.text
+    
+    def go_recaptcha_page(self):
+        MDDialog(
+            title= "Recaptcha test.",
+            text_color= [1,1,1,1],
+            theme_text_color= 'Custom',
+            Image=[
+                Image(
+                    source= 'test_recaptcha.png',
+                    center_x= self.parent.center_x,
+                    center_y= self.parent.center_y
+
+                ),
+
+            ],
+            
+        ),
+
+    def recaptcha_test(self, image):
+        image= [
+            Image(
+                source= 'test_recaptcha.png',
+                center_x= self.parent.center_x,
+                center_y= self.parent.center_y,   
+            ),
+            Image(
+                source= 'test_recaptcha.png',
+                center_x= self.parent.center_x,
+                center_y= self.parent.center_y,   
+            ),
+            Image(
+                source= 'test_recaptcha.png',
+                center_x= self.parent.center_x,
+                center_y= self.parent.center_y,   
+            ),
+            Image(
+                source= 'test_recaptcha.png',
+                center_x= self.parent.center_x,
+                center_y= self.parent.center_y,   
+            ),
+            Image(
+                source= 'test_recaptcha.png',
+                center_x= self.parent.center_x,
+                center_y= self.parent.center_y,   
+            ),
+            Image(
+                source= 'test_recaptcha.png',
+                center_x= self.parent.center_x,
+                center_y= self.parent.center_y,   
+            ),
+            Image(
+                source= 'test_recaptcha.png',
+                center_x= self.parent.center_x,
+                center_y= self.parent.center_y,   
+            ),
+        random.choice(image)
+
+        ]
+    def password_not_valid(self, password):
         password = self.root.current_screen.ids.password.text
-        country = self.root.current_screen.ids.country.text
-        if (not '@' and '.' in e_mail) and len(e_mail) < 6:
-            toast("Please enter a valid e-mail.")
-        else:
-            return e_mail
-        if len(full_name) < 5:
-            toast("Please enter yout full name.")
-        else:
-            return full_name
-        if re.search(e_mail, pattern):
-           return True
-        else:
-            toast("Please enter a valid e_mail")
         if len(password) < 8:
-            toast("Please enter a valid password.")
+            alerte = [
+                MDDialog(
+                    title= 'Password not valid',
+                    text_color= [1,1,1,1],
+                    theme_text_color= 'Custom',
+                    text= 'Please consider seeing the examples of good password. (But do not take them as password.)',
+                    font_name= 'Popp',
+                )
+            ]
+
+
+
+    def check_auth_user(self, e_mail):
+        full_name_text = self.root.current_screen.ids.full_name.text
+        age = int(self.root.current_screen.ids.age.text)
+        e_mail = self.root.current_screen.ids.e_mail.text
+        gender = str(self.root.current_screen.gender.text)
+        country = self.root.current_screen.country.ids.text 
+        pattern_verify = '^[a-z 0-9]+[\._]?[a-z 0-9]+[@]\w+[.]\w{2,3}$'
+
+        if len(self.root.current_screen.ids.full_name.text) < 4:
+            toast("Please enter a valid name.")
         else:
-            return password
-        if len(address) < 10:
-            toast("Please enter a valid address.")
+            return True
+        if int(self.root.current_screen.ids.age.text)<16:
+            toast("You need to be aged of at least sixteen-years-old to use this application.See more at RGPD website.")
         else:
-            return address
-        if int(age) < 15:
-            toast("You need to be aged of at least 15 years old to use Reviewin.")
+            return True
+        if self.root.current_screen.ids.gender.text != "M" or "F":
+            toast("You need to mention your gender.")
         else:
-            return int(age)
-        if len(address) < 10 and int(age) < 15 and len(password) < 8 and len(full_name) < 5 and not  "@" or "#" or '&' or '-' or '_' or "+" and not "1" or "2" or "3" or "4" or "5" or "6" or "7" or "8" or "9" in password:
-            toast("Please register correctly ! ")
+            return True
+        if re.search(pattern, e_mail):
+            return True        
         else:
-            return something
-         
-     def create_user(self):
+            toast("Please enter a valid e-mail. If needed, you can check an example of a valid password.")
+        if len(country) < 4:
+            toast("Country invalid.")
+        else:
+            return True
+        if len(self.root.current_screen.ids.full_name.text) < 4 and int(self.root.current_screen.ids.age.text) and self.root.current_screen.ids.gender.text == 'M' or 'F' and re.search(pattern, e_mail) and len(country) < 4:
+            recaptcha_test_succesfull()
+        else:
+            toast("Please sign up correctly. We advice you to see the register model.")
+
+    def go_back_1(self):
+        sm.current = "register"
+    
+    def test(self):
+        if self.root.current_screen.ids.full_name.text == 'something':
+            print(json.dumps(self.root.current_screen.ids.full_name.text))
+        else:
+            print("no")
+
+    def recaptcha_test_succesfull(self):
+        #variables de récupérations du text input
+        full_name = str(self.root.current_screen.ids.full_name.text)
+        gender = str(self.root.current_screen.ids.gender.text)
+        age = str(self.root.current_screen.ids.age.text)
+        country = str(self.root.current_screen.ids.country.text)
+        e_mail = str(self.root.current_screen.ids.e_mail.text)
+        password = str(self.root.current_screen.ids.password.text)
+
+
+        #variable de serializations en objets json
+        data_full_name = json.dumps(str(full_name))
+        data_gender = json.dumps(str(gender))
+        data_age = json.dumps(int(age))
+        data_country = json.dumps(str(country))
+        data_e_mail = json.dumps(str(e_mail))
+
+        #requete post/signup à l'endpoint de l'API.
+        url = 'http://127.0.0.1:2222/test_verification'
+        data = {'full_name':json.dumps(full_name), 'age':json.dumps(age), 'country':json.dumps(country), 'e_mail':json.dumps(e_mail), 'gender':json.dumps(gender), 'password':json.dumps(password)}
+        response = requests.post(url, json=data)
+
+
+
+
+#fonction de secours dont on aura surement pas besoin p
+    def create_user(self):
         full_name = str(self.root.current_screen.ids.full_name.text)
         gender = str(self.root.current_screen.ids.gender.text)
         age = int(self.root.current_screen.ids.age.text)
@@ -201,14 +289,12 @@ class ReviewinApp(MDApp):
 
 
 
-    def win_points(self):
-        screen_1 = self.root.get_screen('user2')
-        points = screen_1.ids.points.text
-        points+=5
-        return points
+        
+#if re.search(pattern,full_name_text):
+    #something()
 
-
-
+        
+#classes_de_tous_les_screens    
 class Splash(Screen):
     def build(self):
         self.title = "Splash"
@@ -220,19 +306,11 @@ class Splash(Screen):
 class Condition(Screen): 
     def build(self):
         self.title = "Conditions d'utilisations"
-        global co
+        global co        
         co = Screen()
         co = Builder.load_file('conditions.kv')
         return co
 
-
-class Register(Screen):
-    def build(self):
-        self.title = "Register part"
-        global re
-        re  = Screen()
-        re  = Builder.load_file("register.kv")
-        return re
 
 
 
@@ -243,7 +321,6 @@ class Accueil(Screen):
         ac = Screen()
         ac =  Builder.load_file('accueil.kv')
         return ac
-
 
 
 class Faq(Screen):
@@ -299,6 +376,7 @@ class CommentInput(Screen):
         comment  = Builder.load_file("commentinput.kv")
         return comment
 
+
 class UserInfo(Screen):
     def build(self):
         self.title = "User Informations"
@@ -307,8 +385,25 @@ class UserInfo(Screen):
         UserInfo = Builder.load_file("UserInfo.kv")
         return UserInfo
 
+
+class Recaptcha(Screen):
+    def build(self):
+        self.title = "Recaptcha"
+        global rec
+        rec  = Screen()
+        rec = Builder.load_file("recaptcha.kv")
+        return rec
+
+
+
 if __name__=="__main__":
     ReviewinApp().run()
+
+
+
+
+
+
 
 
 
