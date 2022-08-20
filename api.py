b725def831b9c1831292ef88b36b136fdabca088
A1000 <105562940+AyoubSemsar@users.noreply.github.com>
2022-08-20 14:58:16 +0200

    Update api.py
    test fait avec vérfication de l'e_mail

--- api.py
+++ api.py
@@ -30,20 +30,27 @@ async def sign_up(info_user: User_register):
     return {"Status":"Done"}
 
 
-@api.post('/test_verification')
+@api.post('/users')
 async def test_verification(info_user: User_register):
     db = couchdb.Database('http://admin:kolea21342@localhost:5984/my_database_2')
     info_user = info_user.dict()
-    if len(info_user["password"]) < 8:
-        return {"bad":"password"}
+    res = requests.get('http://admin:kolea21342@localhost:5984/db_reviewin_2/9f86611934b286b7ae19c35c6c000409')
+    print(res.text)
+    if len(info_user["password"]) < 8 and info_user['e_mail'] in res.text:
+        return {"bad":"password","e_mail_user":"in db"}
     else:
         db.save(info_user)
-        return {"Status":"Done"}
+        return {"Status":"Done", "e_mail_user":"not in db"}
+
 
 @api.get('/login')
 async def login_user():
-    return {"Hello":"World"}
-
-
+    res = requests.get('http://admin:kolea21342@localhost:5984/db_reviewin_2/9f86611934b286b7ae19c35c6c000409')
+    print(res.text)
+    if "John Doe" in res.text:
+        print("It's on db")
+    else:
+        return {"Status":"Not Done"}
+    
 if __name__ == '__main__':
     uvicorn.run(api, host= '127.0.0.1', port= 2222)
