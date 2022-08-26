eabf96947081447fa42de4d2f384175acbe7dce4
A1000 <105562940+AyoubSemsar@users.noreply.github.com>
2022-08-26 12:01:57 +0200

    Update api.py

--- api.py
+++ api.py
@@ -10,12 +10,19 @@ from fastapi import Request
 from fastapi.responses import HTMLResponse
 from fastapi.staticfiles import StaticFiles
 from fastapi.templating import Jinja2Templates
+import recaptcha_2 as _recaptcha_2
+import fastapi.responses as _responses 
+import random as _random
+import captcha 
+from captcha.image import ImageCaptcha
 
 api = FastAPI() #on instancie 
 
-api.mount("/static", StaticFiles(directory="static"), name="static")
+class UserLogin(BaseModel):
+    full_name: str
+    e_mail: str
+    password: str
 
-templates = Jinja2Templates(directory="templates")
 
 class User_register(BaseModel):
     full_name: str
@@ -56,10 +63,39 @@ async def login_user():
     else:
         return {"Status":"Not Done"}
 
-#retourne la page sign up. Le sign up est géré par le serveur de l'API.
+templates = Jinja2Templates(directory="static")
+api.mount("/static", StaticFiles(directory="static"), name="static")
+
 @api.get('/signup', response_class= HTMLResponse)
-async def return_html():
-    return templates.TemplateResponse('index_.html')
+async def return_html(request: Request):
+    context = {'request':request}
+    return templates.TemplateResponse('index_.html', context)
+
+@api.post('/test')
+async def test():
+    return {"Test":"not done"}
+
+@api.get("/recaptcha")
+def get_image():
+    image_path = _recaptcha_2.select_random("web")
+    return _responses.FileResponse(image_path)
+
+@api.get('/captcha')
+def return_image():
+    ac  = ['a', 'b', 'c', 'd', 'e', 'f', 'g', 'h', 'i', 'j', 'k', 'l', 'm', 'n', 'o', 'p', 'q', 'r', 's', 't', 'u', 'v', 'w', 'x', 'y', 'z','1','2','3','4','5','6','7','8','9','10','A','B','C','D','E','F','G','H','I','J','K','L','M','N','O','P','Q','R','S','T','U','V','W','X','Y','Z']
+    a = _random.choice(ac)
+    b = _random.choice(ac)
+    c = _random.choice(ac)
+    d = _random.choice(ac)
+    f = _random.choice(ac)
+    g = _random.choice(ac)
+    h = _random.choice(ac)
+    random_string = a + b + c + d + f + g + h 
+    random_source = random_string + '.png'
+    image = ImageCaptcha(width=100, height=90)
+    gen = image.generate_image(random_string)
+    gen_1 = gen.save(random_source)
+    return _responses.FileResponse(random_source)
     
 if __name__ == '__main__':
-    uvicorn.run(api, host= '127.0.0.1', port= 2222)
+    uvicorn.run(api, host= '127.0.0.1', port= 8080)
