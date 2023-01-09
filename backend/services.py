a6a57cac779e39833aeeaf0a7b033f7d5eac71d7
locness3 <37651007+locness3@users.noreply.github.com>
2023-01-09 14:12:40 +0100

    [api-server] (non testé uwu) simplifie la création de la liste de pays

--- backend/services.py
+++ backend/services.py
@@ -4,11 +4,10 @@ def services(database_name, payload: dict):
 
 import pycountry
 from pycountry import countries
-list_of_countries = []
-for i in range(len(pycountry.countries)):
-    list_of_countries.append(list(pycountry.countries)[i].name)
+
 final_list_of_countries_imported_uwu = []
-for i in range(len(list_of_countries)):
-    final_list_of_countries_imported_uwu.append(list_of_countries[i].upper())
+for i in range(len(pycountry.countries)):
+    list_of_countries.append(list(pycountry.countries)[i].name.upper())
+    
 print(final_list_of_countries_imported_uwu)
 
