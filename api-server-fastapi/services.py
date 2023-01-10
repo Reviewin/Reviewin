1828cef94524e5a89e46d7ffa8a1e79afcb41ede
A1000 <ayoub_semsar@yahoo.com>
2023-01-10 22:36:47 +0100

    Hey
    Hey première utilisation de github desktop

---
+++ api-server-fastapi/services.py
@@ -0,0 +1,13 @@
+
+def services(database_name, payload: dict):
+    database_name.save(payload)
+
+import pycountry
+from pycountry import countries
+
+final_list_of_countries_imported_uwu = []
+for i in range(len(pycountry.countries)):
+    list_of_countries.append(list(pycountry.countries)[i].name.upper())
+    
+print(final_list_of_countries_imported_uwu)
+
