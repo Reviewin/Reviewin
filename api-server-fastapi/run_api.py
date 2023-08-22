b39ac248453d5f2222d8a3ddb33dccbcf2721ff4
A1000 <ayoub_semsar@yahoo.com>
2023-08-22 20:00:29 +0200

    [rename]
    rename

--- api-server-fastapi/run_api.py
+++
@@ -1,7 +0,0 @@
-#import os
-#os.system("uvicorn api:api --reload")
-import requests
-import couchdb 
-liste_email = ['Billel', 'soundous', 'ayoub', 'soundouse', 'tarek']
-response = requests.get(f"http://")
-#création d'email full inscriptions
